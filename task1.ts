08387e947bace265b244ced2ba2923db0d321409
MaryiaSiarhei <maryia.siarhei@gmail.com>
2026-09-03 20:53:38 +0300

    feat: task1 done

--- task1.ts
+++ task1.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 const name: string = "Александр";
 
 const text: string = `Я, ${name}, учу TYPESCRIPT`;
@@ -6,3 +5,12 @@ const text: string = `Я, ${name}, учу TYPESCRIPT`;
 //Написать программу, которая выводит text в нижнем регистре с вашим именем, если имя не совпадает, то заменить его
 
 const yourName: string = "Настя";
+if (text.includes(yourName)) {
+  console.log(text.toLowerCase());
+} else {
+  const newText: string = text
+    .slice(0, text.indexOf(name))
+    .concat(yourName)
+    .concat(text.slice(text.indexOf(name) + name.length));
+  console.log(newText);
+}
